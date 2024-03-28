e868159139f31d4bc61f1b8037fe6faef9986a63
Geraud Greyling <96425734+geraud22@users.noreply.github.com>
2024-03-28 12:03:43 +0200

    Added the parseBatteryPacket() function. This splits the byte array as outlined by the S2100 Docs and assigns the values to appropriate keys.

--- chirpstackv4_s2100_decoder.js
+++ chirpstackv4_s2100_decoder.js
@@ -12,6 +12,21 @@ function determinePacketType(bytes){
   return packetType;
 }
 
+function parseBatteryPacket(bytes) {
+  var parsedInformation = {};
+  var batteryLevel = bytes[1]; // Byte 2
+  var softwaresubset = bytes.slice(2,4); // Bytes 3 and 4
+  var hardwaresubset = bytes.slice(4, 6); // Bytes 5 and 6
+  var measurementInterval = bytes.slice(6,8); // Bytes 7 and 8 
+  var reserved = bytes.slice(8); // Bytes 9 and 10
+  parsedInformation.batteryLevel = batteryLevel
+  parsedInformation.softwareVersion = toString(parseInt(softwaresubset[0], 16))+"."+toString(parseInt(softwaresubset[1], 16));
+  parsedInformation.hardwareVersion = toString(parseInt(hardwaresubset[0], 16))+"."+toString(parseInt(hardwaresubset[1], 16));
+  parsedInformation.measurementInterval = parseInt(measurementInterval, 16);
+  parsedInformation.reservedValue = parseInt(reserved, 16);
+  return parsedInformation; 
+}
+
 // Decode uplink function.
 //
 // Input is an object with the following fields:
@@ -22,8 +37,16 @@ function determinePacketType(bytes){
 // Output must be an object with the following fields:
 // - data = Object representing the decoded payload.
 function decodeUplink(input) {
+  var decoded_object = {};
   var bytes = input.bytes;
   var packetType = determinePacketType(bytes)
+
+  switch (packetType){
+    case 31:
+    case 30:
+    case 39:
+      decoded_object.batteryInformation = parseBatteryPacket(bytes);
+  }
   
   return {
       data: {
