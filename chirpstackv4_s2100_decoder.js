95f01b53448f37d9efcbf1db8d3edad9497db59a
Geraud Greyling <96425734+geraud22@users.noreply.github.com>
2024-04-02 08:35:39 +0200

    Significant change in approach. Now making use of device variables, assigned server side, to determine what kind of packet to decode. New approach means each time the organization adds a new sensor to an S2100 DataLogger, this codec will have to be updated to parse the new sensor's data. A clear risk to scalability. However, the benefit is more control over the data structure that reaches the database.

--- chirpstackv4_s2100_decoder.js
+++ chirpstackv4_s2100_decoder.js
@@ -10,92 +10,87 @@
 function decodeUplink(input) {
   var decodedObject = {};
   var bytes = input.bytes;
-  var packetType = determinePacketType(bytes)
+  var nodeType = variables.nodeType;
+  var isBatteryPacket = determineBatteryPacket(bytes)
 
-  switch (packetType){
-    case 31:
-      decodedObject.decodedMeasurements = parseSinglePacket(bytes);
-    case 30:
-      decodedObject.decodedMeasurements = parseMutliplePackets(bytes);
-    case 39:
-      decodedObject.batteryInformation = parseBatteryPacket(bytes);
+  if (isBatteryPacket){
+    decodedObject.batteryInformation = parseBatteryPacket(bytes);
+  } else {
+    switch (nodeType){
+      case 31:
+        decodedObject.decodedMeasurements = parseSinglePacket(bytes);
+        break
+      case "sensecapweathersensor":
+        decodedObject.decodedMeasurements = parseSenseCapWeatherSensor(bytes);
+        break
+      case 39:
+        decodedObject.batteryInformation = parseBatteryPacket(bytes);
+        break
+    }
   }
   
   return {
       data: decodedObject
     };
+}
+  
+function determineBatteryPacket(bytes){
+  var isBatteryPacket = false;
+  if (bytes[0] != 0x39){
+    return isBatteryPacket;
+  } else {
+    isBatteryPacket = true;
+    return isBatteryPacket;
   }
+}
+  
+function parseBatteryPacket(bytes) {
+  var parsedInformation = {}; 
+  parsedInformation.batteryLevel = parseInt(input2HexString(bytes[1]), 16); // Byte 2
+  parsedInformation.softwareVersion = toString(parseInt(input2HexString(bytes[2]), 16))+"."+toString(parseInt(input2HexString(bytes[3]), 16)); // Bytes 3 and 4
+  parsedInformation.hardwareVersion = toString(parseInt(input2HexString(bytes[4]), 16))+"."+toString(input2HexString(parseInt(bytes[5]), 16)); // Bytes 5 and 6
+  parsedInformation.measurementInterval = parseInt(input2HexString(bytes.slice(6,8)), 16); // Bytes 7 and 8 
+  parsedInformation.reservedValue = parseInt(input2HexString(bytes.slice(8)), 16); //Bytes 9 and 10
+  
+  return parsedInformation; 
+}
 
-  function input2HexString (arrBytes) {
-    var str = ''
-    for (var i = 0; i < arrBytes.length; i++) {
-        var tmp
-        var num = arrBytes[i]
-        if (num < 0) {
-            tmp = (255 + num + 1).toString(16)
-        } else {
-            tmp = num.toString(16)
-        }
-        if (tmp.length === 1) {
-            tmp = '0' + tmp
-        }
-        str += tmp
-    }
-    return str
-  }
+function parseSinglePacket(bytes) {
+  var decodedPacket = {};
+  decodedPacket.measurement1 = parseInt(input2HexString(bytes.slice(3, 7)), 16); // Byte 4-7
+  decodedPacket.measurement2 = parseInt(input2HexString(bytes.slice(7)), 16); // Byte 8-11
   
-  function determinePacketType(bytes){
-    var packetType = 0;
-    if (bytes[0] == 0x31) {
-      packetType = 31;
-    } else if (bytes[0] == 0x30) {
-      packetType = 30;
-    } else if (bytes[0] == 0x39) {
-      packetType = 39;
-    } else {
-      packetType = 0;
-    }
-    return packetType;
+  return decodedPacket;
+}
+
+function parseSenseCapWeatherSensor(bytes) {
+  var decodedPacket = {};
+  decodedPacket.temperature = parseInt(input2HexString(bytes.slice(3, 7)), 16); // Byte 4-7
+  decodedPacket.humidity = parseInt(input2HexString(bytes.slice(7, 11)), 16); // Byte 8-11
+  decodedPacket.barometricPressureMSB = parseInt(input2HexString(bytes.slice(13, 17)), 16); // Byte 14-17
+  decodedPacket.barometricPressureLSB = parseInt(input2HexString(bytes.slice(17, 21)), 16); // Byte 18-21
+  decodedPacket.lightIntensityMSB = parseInt(input2HexString(bytes.slice(24, 28)), 16); // Byte 25-28
+  decodedPacket.lightIntensityLSB = parseInt(input2HexString(bytes.slice(28, 32)), 16); // Byte 29-32
+  return decodedPacket;
+}
+
+function input2HexString (arrBytes) {
+  var str = ''
+  for (var i = 0; i < arrBytes.length; i++) {
+      var tmp
+      var num = arrBytes[i]
+      if (num < 0) {
+          tmp = (255 + num + 1).toString(16)
+      } else {
+          tmp = num.toString(16)
+      }
+      if (tmp.length === 1) {
+          tmp = '0' + tmp
+      }
+      str += tmp
   }
-  
-  function parseBatteryPacket(bytes) {
-    var parsedInformation = {}; 
-    parsedInformation.batteryLevel = parseInt(input2HexString(bytes[1]), 16); // Byte 2
-    parsedInformation.softwareVersion = toString(parseInt(input2HexString(bytes[2]), 16))+"."+toString(parseInt(input2HexString(bytes[3]), 16)); // Bytes 3 and 4
-    parsedInformation.hardwareVersion = toString(parseInt(input2HexString(bytes[4]), 16))+"."+toString(input2HexString(parseInt(bytes[5]), 16)); // Bytes 5 and 6
-    parsedInformation.measurementInterval = parseInt(input2HexString(bytes.slice(6,8)), 16); // Bytes 7 and 8 
-    parsedInformation.reservedValue = parseInt(input2HexString(bytes.slice(8)), 16); //Bytes 9 and 10
-    
-    return parsedInformation; 
-  }
-  
-  function parseSinglePacket(bytes) {
-    var decodedPacket = {};
-    decodedPacket.measurement1 = parseInt(input2HexString(bytes.slice(3, 7)), 16); // Byte 4-7
-    decodedPacket.measurement2 = parseInt(input2HexString(bytes.slice(7)), 16); // Byte 8-11
-    
-    return decodedPacket;
-  }
-  
-  function parseMutliplePackets(bytes) {
-    var decodedPacket = {};
-    if (bytes[11] === 0x32 && bytes[12] === 0x34){
-      decodedPacket.measurement1 = parseInt(input2HexString(bytes.slice(3, 7)), 16); // Byte 4-7
-      decodedPacket.measurement2 = parseInt(input2HexString(bytes.slice(7, 11)), 16); // Byte 8-11
-      decodedPacket.measurement3 = parseInt(input2HexString(bytes.slice(13, 17)), 16); // Byte 14-17
-      decodedPacket.measurement4 = parseInt(input2HexString(bytes.slice(17, 21)), 16); // Byte 18-21
-      decodedPacket.measurement5 = parseInt(input2HexString(bytes.slice(24, 28)), 16); // Byte 25-28
-      decodedPacket.measurement6 = parseInt(input2HexString(bytes.slice(28, 32)), 16); // Byte 29-32
-    } else {
-      decodedPacket.measurement1 = parseInt(input2HexString(bytes.slice(3, 7)), 16); // Byte 4-7
-      decodedPacket.measurement2 = parseInt(input2HexString(bytes.slice(7, 11)), 16); // Byte 8-11
-      decodedPacket.measurement3 = parseInt(input2HexString(bytes.slice(14, 18)), 16); // Byte 15-18
-      decodedPacket.measurement2 = parseInt(input2HexString(bytes.slice(18-22)), 16); // Byte 19-22
-    }
-    
-    return decodedPacket;
-  }
-  
+  return str
+}
   // Encode downlink function.
   //
   // Input is an object with the following fields:
